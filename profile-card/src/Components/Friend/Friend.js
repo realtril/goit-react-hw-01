8f9e3033cf6760e1c112f309aba6ab22607d7505
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-07-14 18:04:39 +0300

    changed appearance

--- profile-card/src/Components/Friend/Friend.js
+++ profile-card/src/Components/Friend/Friend.js
@@ -1,16 +1,19 @@
 import React from "react";
 import styles from "../FriendsList/FriendList.module.css";
-
+// const style = [styles.offline, styles.online];
 const Friend = ({ avatar, name, isOnline }) => {
   return (
     <li className={styles.friend}>
-      <span
-        className={`${"status"} ${isOnline ? styles.online : styles.offline}`}
-      >
-        ....
-      </span>
       <span className={styles.status}>{isOnline}</span>
-      <img className={styles.avatar} src={avatar} alt="" width="48" />
+      <img
+        className={[
+          styles.avatar,
+          isOnline ? styles.online : styles.offline,
+        ].join(" ")}
+        src={avatar}
+        alt=""
+        width="48"
+      />
       <p className={styles.name}>{name}</p>
     </li>
   );
